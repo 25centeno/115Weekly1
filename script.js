253f5246503ef027a5a0c086eb0ba7db37af26ba
25centeno <victoria76969@gmail.com>
2025-10-07 00:21:51 -0400

    Fix3

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var Value1 = 0
 var Value2 = ""
 var Value3 = 0
 var Value4 = 0
+var Value5 = 0
 
 var Value1IsDone = 0;
-var Value2IsDone = 0;
+var CalcRan = 0;
+var IsZero = 0;
 
 const Output = document.getElementById("Output")
 
@@ -17,7 +19,7 @@ function Calculator(Arg){
         Value4 = 0
 
         Value1IsDone = 0
-        Value2IsDone = 0
+        CalcRan = 0
 
         Output.innerHTML = "0"
     }
@@ -69,7 +71,7 @@ function Calculator(Arg){
         Value1IsDone = 1;
 
         if (Arg2 == "Plus"){
-            Value2 = "Plus"
+            Value2 = "Add"
         } else if (Arg2 == "Subtract"){
             Value2 = "Subtract"
         } else if (Arg2 == "Times"){
@@ -80,8 +82,6 @@ function Calculator(Arg){
     }
 
     function CalcV3(Arg2){
-        Value2IsDone = 1;
-
         if (Arg2 == "1"){
                 Value3 = Value3+"1"
             } else if (Arg2 == "2"){
@@ -120,6 +120,10 @@ function Calculator(Arg){
                 Value3 = 0;
             }
 
+            if (Value3 == 0 && Arg2 == "0"){
+                IsZero = 1;
+            }
+
             Value3 = parseFloat(Value3)
             Output.innerHTML = Value3;
     }
@@ -129,20 +133,33 @@ function Calculator(Arg){
             Value4 = parseFloat(Value1)*parseFloat(Value3)
         } else if (Value2 == "Divide" && Value3 != 0){
             Value4 = parseFloat(Value1)/parseFloat(Value3)
+        } else if (Value2 == "Divide" && Value3 == 0){
+            Value4 = "ERROR"
         } else if (Value2 == "Subtract"){
             Value4 = parseFloat(Value1)-parseFloat(Value3)
         } else if (Value2 == "Add"){
             Value4 = parseFloat(Value1)+parseFloat(Value3)
         }
 
+        Value5 = Value3
         Value1 = Value4
         Value3 = 0
         Output.innerHTML = Value4 
+
+        CalcRan = 1
     }
 
 
     if (Arg == "Equal") {
-        Calculate();
+        if (CalcRan == 1 && Value3 == 0 && IsZero == 1){
+            IsZero = 0;
+            Calculate();
+        } else if (CalcRan == 1 && Value3 == 0 && IsZero == 0){
+            Value3 = Value5
+            Calculate();
+        } else {
+            Calculate();
+        }
     } else if (Arg == "Dot"){
         if (Value1IsDone == 1){
             CalcV3("Dot")
